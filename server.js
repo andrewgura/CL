b198be3a550c92fdefd825aa1147e1dcbda1ed79
Joshua Veal-Briscoe <jkv2c9@mail.umsl.edu>
2017-12-20 20:34:38 -0600

    App now gets all of a user's followers.

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
+var request = require('request')
 
 app.get('/', function(req, res){
-   res.send("Hello world!");
+   res.send("Hello world!")
 
-});
+})
 
-app.get('/:userId([a-zA-Z0-9_]+)\/followers', function(req, res){
-   res.send(req.params);
+app.get('/:username([a-zA-Z0-9_]+)\/followers', function(req, res, next){
+   var usersFollowersURL = 'https://api.github.com/users/' + req.params.username + '/followers'
+   var options = {
+     url: usersFollowersURL,
+     headers: {
+       'User-Agent': 'vealjoshua'
+     }
+   }
+   request.get(options, function (error, response, body) {
+     console.log('error:', error); // Print the error if one occurred
+     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+     // res.send(body);
+     res.locals.followers = body;
+     next()
+   })
 
-});
+}, function(req, res) {
+  res.send(res.locals.followers)
+  console.log("This is the next function.");
+})
 
 
 app.listen(8080);
